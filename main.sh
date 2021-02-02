93b459061c86a94b0fff58a339b88a436da45ac5
Kartikey Tewari <kartikeya30@gmail.com>
2021-02-02 18:34:02 +0530

    Refactored the main.sh shell script

--- main.sh
+++ main.sh
@@ -1,3 +1,10 @@
+#!/usr/local/bin/zsh
+function press_key()
+{
+    echo "Press key to continue....";
+    read alpha;
+}
+
 figlet "Fingerprint Searcher"
 echo "Warning: Running this script will delete all previous data and metrics."
 echo "Do you still want to proceed? (y/n)"
@@ -7,19 +14,23 @@ if [[ $var = y ]]
 then
     echo "Building the project"
     ./build.sh 2>/dev/null
-    read alpha
+    press_key
+
     echo "Cleaning all data"
     cd data_wrangler
     ./dw_passer.sh 2>/dev/null
-    read alpha
+    press_key
+
     echo "Passing data through template"
     cd ../template
     ./feeder.sh 2>/dev/null
-    read alpha
+    press_key
+
     echo "Generating metrics"
     cd ../metric_generator
     ./metric_generator.sh 2>/dev/null
-    read alpha
+    press_key
+
     echo "Building and searching on Wavelet tree"
     cd ../wt
     ./gen_search.sh 2>/dev/null

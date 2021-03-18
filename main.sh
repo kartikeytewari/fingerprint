bb40573a48ae9388e04292476f35ff0b99d47353
Kartikey Tewari <kartikeya30@gmail.com>
2021-03-18 13:11:54 +0530

    refactored main.sh file

--- main.sh
+++ main.sh
@@ -8,6 +8,14 @@ function press_key()
     read alpha;
 }
 
+function process_file()
+{
+    echo "Processing file - " $1
+    echo $(wc -l ./data/raw/$1.txt | awk '{ print $1 }') > ./data/wrangled/$1.wr.txt
+    cat ./data/raw/$1.txt >> ./data/wrangled/$1.wr.txt
+    ./template/eucledian_distance < ./data/wrangled/$1.wr.txt >> ./data/database/1.txt
+}
+
 figlet "Fingerprint Searcher"
 echo "Warning: Running this script will delete all previous data and metrics."
 echo "Do you still want to proceed? (y/n)"
@@ -16,10 +24,7 @@ read var
 if [[ $var = y ]]
 then
     for ((i=1;i<=$(ls -l ./data/raw |wc -l)-1;i++)) do
-    echo "Processing file - " $i
-    echo $(wc -l ./data/raw/$i.txt | awk '{ print $1 }') > ./data/wrangled/$i.wr.txt
-    cat ./data/raw/$i.txt >> ./data/wrangled/$i.wr.txt
-    ./template/eucledian_distance < ./data/wrangled/$i.wr.txt >> ./data/database/1.txt
+    process_file $i
     done
     press_key
 
@@ -27,20 +32,11 @@ then
     ./metric_generator/main < ./data/database/1.txt
     press_key
 
-    echo "Building and searching on Wavelet tree"
-    # clean the data
-    echo "Cleaning the data"
+    echo "Making the wavellet tree and performing search"
     echo $(wc -l ./data/auth_candidate/file.txt | awk '{ print $1 }') > temp.txt
     cat ./data/auth_candidate/file.txt >> temp.txt
-
-    # pass the data through the template
-    echo "Passing the data through the template"
     ./template/eucledian_distance < temp.txt > param.txt
-
-    # make tree and search the value
-    echo "Generating the tree and performing search"
     ./wt/serial < ./data/database/1.txt
-    cd ..
 else
     echo "Process terminated."
 fi
